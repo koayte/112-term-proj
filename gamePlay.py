36e4f39d8d6351c402508e5110b9556cd28e3708
koayte <95264669+koayte@users.noreply.github.com>
2023-11-21 09:37:03 -0500

    Fixed bug to store mouseX and mouseY without onMouseMove.

--- gamePlay.py
+++ gamePlay.py
@@ -6,6 +6,7 @@ def onAppStart(app):
     app.width = 1920
     app.height = 1080 
     app.gridSize = 40
+    app.stepsPerSecond = 50
 
     # player
     app.playerX = app.width//2
@@ -23,6 +24,8 @@ def onAppStart(app):
     app.rangeLineTwoX = 0
     app.rangeLineTwoY = 0
     app.rangeCoords = [app.playerX, app.playerY, app.rangeLineOneX, app.rangeLineOneY, app.rangeLineTwoX, app.rangeLineTwoY]
+    app.mouseX = 0
+    app.mouseY = 0
 
 def redrawAll(app):
     drawPolygon(*app.rangeCoords, fill='blue', opacity=80)
@@ -38,27 +41,26 @@ def onKeyPress(app, key):
 def onKeyHold(app, keys):
     # navigation
     if 'w' in keys and 's' not in keys:
-        app.playerY -= 3
+        app.playerY -= 1
     elif 'w' not in keys and 's' in keys:
-        app.playerY += 3
+        app.playerY += 1
     if 'a' in keys and 'd' not in keys:
-        app.playerX -= 3
+        app.playerX -= 1
     elif 'a' not in keys and 'd' in keys:
-        app.playerX += 3
+        app.playerX += 1
 
-def onMouseMove(app, mouseX, mouseY):
-    ######## PROBLEM WITH IF MOUSE IS NOT MOVING, I DON'T WANT THE RANGE TO INCREASE
+def mouseToAim(app):
     # aim 
-    straightDist = distance(app.playerX, app.playerY, mouseX, mouseY)
-    fraction = (mouseX - app.playerX) / straightDist if straightDist != 0 else 1
-    if mouseY > app.playerY: # mouseY is below player 
+    straightDist = distance(app.playerX, app.playerY, app.mouseX, app.mouseY)
+    fraction = (app.mouseX - app.playerX) / straightDist if straightDist != 0 else 1
+    if app.mouseY > app.playerY: # mouseY is below player 
         app.aimDirection = -1*math.acos(fraction)
     else: # mouseY is above player 
         app.aimDirection = math.acos(fraction)
     app.aimLineX = app.playerX + app.aimLength*math.cos(app.aimDirection)
     app.aimLineY = app.playerY - app.aimLength*math.sin(app.aimDirection)
 
-    # triangle range 
+    # triangular range 
     rangeSideLen = app.aimLength / math.cos(app.aimAngle / 2)
     rangeLineOneAngle = app.aimAngle/2 + app.aimDirection 
     app.rangeLineOneX = app.playerX + rangeSideLen*math.cos(rangeLineOneAngle)
@@ -67,6 +69,16 @@ def onMouseMove(app, mouseX, mouseY):
     app.rangeLineTwoX = app.playerX + rangeSideLen*math.cos(rangeLineTwoAngle)
     app.rangeLineTwoY = app.playerY - rangeSideLen*math.sin(rangeLineTwoAngle)
     app.rangeCoords = [app.playerX, app.playerY, app.rangeLineOneX, app.rangeLineOneY, app.rangeLineTwoX, app.rangeLineTwoY]
+
+def onStep(app):
+    mouseToAim(app)
+
+def onMouseMove(app, mouseX, mouseY):
+    ######## PROBLEM WITH IF MOUSE IS NOT MOVING, I DON'T WANT THE RANGE TO INCREASE
+    # aim 
+    app.mouseX = mouseX
+    app.mouseY = mouseY
+
 
 def distance(x1, y1, x2, y2):
     return ((x1-x2)**2+(y1-y2)**2)**0.5

2f521aeb18ddc7a11399348e0e8657076fb63d01
koayte <95264669+koayte@users.noreply.github.com>
2023-11-21 21:03:19 -0500

    Added health bar and ammo bar.

--- gamePlay.py
+++ gamePlay.py
@@ -17,54 +17,62 @@ class Player:
         self.rangeLineOneX = self.rangeLineOneY = self.rangeLineTwoX = self.rangeLineTwoY = 0
         self.rangeCoords = [self.playerX, self.playerY, self.rangeLineOneX, self.rangeLineOneY, self.rangeLineTwoX, self.rangeLineTwoY]
 
+        # ammo and shoot 
+        self.maxAmmo = 1500 
+        self.currAmmo = self.maxAmmo 
+        self.ammoX = self.playerX - self.radius # left 
+        self.ammoY = self.playerY - self.radius*1.5
+
+        # health bar 
+        self.maxHealth = 1500
+        self.currHealth = self.maxHealth 
+        self.healthX = self.playerX - self.radius # left  
+        self.healthY = self.playerY - self.radius*2.2
+
+        # 
+
     def drawPlayer(self, app):
         drawCircle(self.playerX, self.playerY, self.radius, fill='black')
 
     def drawRange(self, app):
         drawPolygon(*self.rangeCoords, fill='yellow', opacity=80)
 
+    def drawHealthBar(self, app):
+        # max health bar
+        drawRect(self.healthX, self.healthY, self.radius*2, 12, align='left', fill='black') 
+        # current health bar  DO THIS 
+        # drawRect(self.healthX, self.healthY, self.currHealth)
+
+    def drawAmmoBar(self, app):
+        drawRect(self.ammoX, self.ammoY, self.radius*2, 12, align='left', fill='green')
+    
+
 
 def onAppStart(app):
-    # map
-    app.width = 1920
-    app.height = 1080 
-    app.gridSize = 40
+    # general 
+    app.width = 1300
+    app.height = 720 
+    app.gridSize = 60
     app.stepsPerSecond = 50
+    app.mouseX = 0
+    app.mouseY = 0
     
 
     # player
-    app.radius = app.gridSize//2 
+    app.radius = app.gridSize/2 
     app.player = Player(app.width/2, app.height/2, app.radius, 80, 0.4, 0)
-    # app.playerX = app.width//2
-    # app.playerY = app.height//2
-    # app.radius = app.gridSize//2
 
-    # aim 
-    # app.aimLength = 80
-    # app.aimAngle = 0.4
-    # app.aimDirection = 0
-    # app.aimLineX = app.aimLength + app.playerX
-    # app.aimLineY = 0
-    # app.rangeLineOneX = 0 
-    # app.rangeLineOneY = 0
-    # app.rangeLineTwoX = 0
-    # app.rangeLineTwoY = 0
-    # app.rangeCoords = [app.playerX, app.playerY, app.rangeLineOneX, app.rangeLineOneY, app.rangeLineTwoX, app.rangeLineTwoY]
-    app.mouseX = 0
-    app.mouseY = 0
 
     # shoot 
 
 
 def redrawAll(app):
-    # drawPolygon(*app.rangeCoords, fill='yellow', opacity=80)
     app.player.drawPlayer(app)
     app.player.drawRange(app)
-    # drawCircle(app.playerX, app.playerY, app.radius, fill='black')
-    # drawLine(app.playerX, app.playerY, app.aimLineX, app.aimLineY)
-    # drawLine(app.playerX, app.playerY, app.rangeLineOneX, app.rangeLineOneY)
-    # drawLine(app.playerX, app.playerY, app.rangeLineTwoX, app.rangeLineTwoY)
-
+    app.player.drawHealthBar(app)
+    app.player.drawAmmoBar(app)
+    
+    
 def onKeyPress(app, key):
     # shoot 
     if key == 'space':
@@ -74,12 +82,21 @@ def onKeyHold(app, keys):
     # navigation
     if 'w' in keys and 's' not in keys:
         app.player.playerY -= 1
+        app.player.ammoY -= 1
+        app.player.healthY -= 1
     elif 'w' not in keys and 's' in keys:
         app.player.playerY += 1
+        app.player.ammoY += 1
+        app.player.healthY += 1
     if 'a' in keys and 'd' not in keys:
         app.player.playerX -= 1
+        app.player.ammoX -= 1
+        app.player.healthX -= 1
     elif 'a' not in keys and 'd' in keys:
         app.player.playerX += 1
+        app.player.ammoX += 1
+        app.player.healthX += 1
+
 
 def mouseToAim(app):
     # aim 
